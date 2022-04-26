5d428ef278b5a6b95b4547ccd48473f9ac3b9467
Aaron <aaron-po@hotmail.co.uk>
2022-04-25 21:36:40 -0500

    last minute update

--- views/html/vendedor_inc/ayuda_y_soporte.php
+++ views/html/vendedor_inc/ayuda_y_soporte.php
@@ -1,7 +1,8 @@
-<div class="col" style="font-size: 22px;  margin-top: 10px;">
-  Como usar Vaira si eres vendedor
-  <br>
-  <iframe width="760" height="515"  src="https://www.youtube.com/embed/sm3Z--3WWcU" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen style="margin-top: 15px;"></iframe>
+  <div style="font-size: 22px; margin-top: 10px;">
+    Como usar Vaira si eres vendedor
+    <br>
+    <iframe width="760" height="515"  src="https://www.youtube.com/embed/sm3Z--3WWcU" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen style="margin-top: 15px;"></iframe>
+  </div>
 </div>
 <div class = "col" style="font-size: 22px;  margin-top: 57px;">
   <div class="card" style="width: 15rem;">
