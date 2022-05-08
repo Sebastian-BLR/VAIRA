b63e3b10331c0300fdbcb83defe539e2200ea711
AndreaEspinosa26 <A01423636@itesm.mx>
2022-05-08 11:29:42 -0500

    NavBar: Icono de Vaira eacomodado COMPLETADO

--- Vendedor/inc/header.php
+++ Vendedor/inc/header.php
@@ -1,7 +1,7 @@
 <div class="row" >
     <nav class="navbar" style="background-color: #ff7e2f; height: 10vh;">
     <a class="navbar-brand" href="index.php">
-        <img src="src/image/vairaNav.png"  width="50" height="50" class="d-inline-block align-top" alt="">
+        <img src="src/image/vairaNav.png"  width="50" height="50" style="margin-left: 8px;margin-top: -5px" class="d-inline-block align-top" alt="">
     </a>
     <form class="d-flex"  action="../services/cerrar.php">
         <button class="btn btn-outline-dark cerrarSesionBtn" type="submit">Cerrar Sesion</button>
