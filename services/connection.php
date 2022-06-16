43099b451082e5be1c2f01c0c0170c2a128221d1
Sebastian BLR <blademirlabra@gmail.com>
2022-06-16 15:51:20 -0500

    Se agregarion las credenciales para la cominicacion con la base de datos

--- services/connection.php
+++ services/connection.php
@@ -5,12 +5,7 @@
     //$password = "root";
     $password = "Ekq3l#pE6qYr";
     $dbname = "naatika1_db_vaira";
-    
-// ! DEVELOPMENT DB
-    // $servername = "db-vaira-2022.cgupkyny0avi.us-west-1.rds.amazonaws.com";
-    // $username = "root";
-    // $password = "dbVaira2022";
-    // $dbname = "db_vaira";
+
 
     try {
         $pdo = new PDO('mysql:host='.$servername.';dbname='.$dbname, $username, $password, array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
